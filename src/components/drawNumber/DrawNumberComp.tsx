8da264f27d845b78e25cf7bc1c20efb7e7785371
Tony <gogo6570@gmail.com>
2024-05-09 18:57:27 +0900

    feat: 추첨번호 생성기능 구현

--- src/components/drawNumber/DrawNumberComp.tsx
+++ src/components/drawNumber/DrawNumberComp.tsx
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 interface LottoGeneratorProps {
   numSets: number;
 }
 
-interface LottoGeneratorState {
-  lottoSets: number[][];
-  generated: boolean;
-}
+const DrawNumberComp: React.FC<LottoGeneratorProps> = ({ numSets }) => {
+  const [lottoSets, setLottoSets] = useState<number[][]>(Array.from({ length: numSets }, () => [0, 0, 0, 0, 0, 0]));
+  const [generated, setGenerated] = useState<boolean>(false);
 
-class DrawNumberComp extends Component<LottoGeneratorProps, LottoGeneratorState> {
-  constructor(props: LottoGeneratorProps) {
-    super(props);
-    // 초기 상태 설정: 5세트의 ??로 된 번호와 번호 생성 여부
-    this.state = {
-      lottoSets: Array.from({ length: this.props.numSets }, () => [0, 0, 0, 0, 0, 0]),
-      generated: false,
-    };
-  }
-
-  generateRandomNumbers = (): number[] => {
+  const generateRandomNumbers = (): number[] => {
     const lottoNumbers: number[] = [];
     while (lottoNumbers.length < 6) {
       const randomNumber = Math.floor(Math.random() * 45) + 1;
@@ -30,62 +20,179 @@ class DrawNumberComp extends Component<LottoGeneratorProps, LottoGeneratorState>
     return lottoNumbers;
   };
 
-  // 번호 뽑기 버튼 클릭 시 호출되는 함수
-  handleButtonClick = () => {
-    // 번호가 생성되지 않은 상태일 때만 번호 생성하도록 변경
-    if (!this.state.generated) {
-      // 새로운 번호 생성
-      const newLottoSets: number[][] = [];
-      for (let i = 0; i < this.props.numSets; i++) {
-        newLottoSets.push(this.generateRandomNumbers());
-      }
-      // 생성된 번호와 버튼 텍스트 업데이트
-      this.setState({
-        lottoSets: newLottoSets,
-        generated: true, // 번호가 생성되었음을 표시
-      });
+  const handleButtonClick = () => {
+    if (!generated) {
+      const newLottoSets = Array.from({ length: numSets }, () => generateRandomNumbers());
+      setLottoSets(newLottoSets);
+      setGenerated(true);
     } else {
-      // 번호가 생성된 상태일 때는 다시 뽑기 기능 수행
-      // 여기서는 추가적인 기능 없이 번호만 다시 생성
-      const newLottoSets: number[][] = [];
-      for (let i = 0; i < this.props.numSets; i++) {
-        newLottoSets.push(this.generateRandomNumbers());
-      }
-      // 생성된 번호 업데이트
-      this.setState({
-        lottoSets: newLottoSets,
-      });
+      const newLottoSets = Array.from({ length: numSets }, () => generateRandomNumbers());
+      setLottoSets(newLottoSets);
     }
   };
 
-  componentDidMount() {
-    // 컴포넌트가 마운트된 후에는 번호 생성하지 않고 초기화만 수행
-    this.setState({
-      generated: false, // 번호 생성 여부 초기화
-    });
-  }
-
-  render() {
-    const { lottoSets, generated } = this.state;
-
-    return (
-      <div>
-        <h2>Lotto Numbers</h2>
-        {lottoSets.map((lottoSet, index) => (
-          <div key={index}>
-            <strong>Set {index + 1}:</strong>{' '}
-            {lottoSet.map((number, index) => (
-              <span key={index}>{generated ? number : '??'} </span>
-            ))}
+  return (
+    <Container>
+      <Header>
+        <div>
+          <div className="imgLogo">
+            <img src="gamelogo.png" alt="" />
+            <h3>LOTTO</h3>
+            <h4>00/00</h4>
+            <img src="QRCode.png" alt="" />
           </div>
-        ))}
-        {/* 번호 뽑기 버튼 */}
-        <button onClick={this.handleButtonClick}>
+        </div>
+        <div>
+          <h1>제 0000 회</h1>
+          <p>발행일 : 0000 / 00 / 00 (토) 18:20:10</p>
+          <p>추첨일 : 0000 / 00 / 00 (토) HR :20987</p>
+          <p>0000 0000 0000 0000 0000 0000</p>
+          <p>12345678ABCDE 0000000/000000</p>
+        </div>
+        <Button className="button" onClick={handleButtonClick}>
           {generated ? '다시 뽑기' : '번호 뽑기'}
-        </button>
-      </div>
-    );
+        </Button>
+      </Header>
+      <Section>
+        {lottoSets.map((lottoSet, index1) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <NumBoxRow className="numboxRow" key={index1}>
+            <span>{String.fromCharCode(65 + index1)}</span>
+            <h3>자 동</h3>
+            {lottoSet.map((number, index2) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <NumberContainer key={`${index1}-${index2}`}>
+                <div>{generated ? '' : '??'}</div>
+                <LottoNumber className="lotteryNum">{generated ? number : ''}</LottoNumber>
+              </NumberContainer>
+            ))}
+          </NumBoxRow>
+        ))}
+      </Section>
+      <Footer>
+        <div className="price">
+          <span>금액</span>
+          <span>5000 Won</span>
+        </div>
+        <p className="footerNum">0000 0000 0000 0000 0000 0000</p>
+        <div className="qrImgBox">
+          <img src="footerQrImg.png" alt="" />
+        </div>
+      </Footer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: auto;
+  width: 50%;
+  height: 100%;
+  padding: 20px;
+  border: 2px solid #000;
+  border-radius: 10px;
+  background-color: #fff;
+`;
+
+const Header = styled.div`
+  background: linen;
+  padding-top: 3rem;
+  position: relative;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
   }
-}
+
+  h3 {
+    position: relative;
+    font-size: 2.3rem;
+    z-index: 1;
+  }
+
+  h3::before {
+    position: absolute;
+    content: 'LOTTO';
+    bottom: -0.4rem;
+    right: -0.4rem;
+    color: #fbd9e2;
+    opacity: 0.6;
+    z-index: -1;
+  }
+
+  h4 {
+    align-self: flex-end;
+    padding-bottom: 0.5rem;
+  }
+
+  img {
+    width: 5rem;
+    height: 5rem;
+    margin-right: 2rem;
+  }
+
+  img:nth-child(4) {
+    width: 5rem;
+    height: 5rem;
+    margin-left: 1rem;
+  }
+
+  div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const Button = styled.button`
+  position: absolute;
+  bottom: 0;
+  right: 0.5rem;
+  padding: 0.2rem;
+  background: lightcoral;
+  border: 2px solid #fadfe6;
+  color: white;
+  font-weight: normal;
+  cursor: pointer;
+  z-index: 9999;
+
+  button:active {
+    transform: scale(0.8);
+    font-size: 90%;
+  }
+`;
+
+const Section = styled.div`
+  background: linen;
+`;
+
+const Footer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const NumBoxRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LottoNumber = styled.div`
+  margin: 0 0.7rem; // 각 번호 사이의 간격 조정
+  font-size: 1.2rem;
+`;
+
+const NumberContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem; /* 숫자의 너비 */
+  height: 2rem; /* 숫자의 높이 */
+  border: 1px solid #000; /* 테두리 스타일 지정 */
+  margin: 0 0.2rem; /* 숫자 사이의 간격 조정 */
+  background-color: white;
+`;
 
 export default DrawNumberComp;
